3cfac4bd5359d0c802254b2f1e2a2a457fdab97e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-10-03 07:37:56 +1000

    Remove gh-pages cache clean step

--- codeship/deploy.sh
+++ codeship/deploy.sh
@@ -2,6 +2,4 @@ git config --global user.email "robot@example.com"
 git config --global user.name "Codeship Robot"
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 
-rm -rf node_modules/gh-pages/.cache
-
 gulp deploy
